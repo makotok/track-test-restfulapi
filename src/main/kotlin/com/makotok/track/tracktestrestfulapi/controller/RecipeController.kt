cdd81eba114a6685110d642b4841e039f8cb12fe
makotok <makotok1123@gmail.com>
2019-06-15 18:47:51 +0900

    Test Track

--- src/main/kotlin/com/makotok/track/tracktestrestfulapi/controller/RecipeController.kt
+++ src/main/kotlin/com/makotok/track/tracktestrestfulapi/controller/RecipeController.kt
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PathVariable
 
 @RestController
 @RequestMapping("/api/recipes")
@@ -21,14 +22,14 @@ class RecipeController(
     val recipeRepository: RecipeRepository
 ) {
 
-    @GetMapping("hello")
-    fun hello() : String {
-        return "hello!"
-    }
-
     @GetMapping
     fun getRecipes(): List<Recipe> {
         return recipeRepository.findAll(Sort(Direction.ASC, "id"))
+    }
+
+    @GetMapping("{id}")
+    fun getRecipe(@PathVariable id: Long): Recipe {
+        return recipeRepository.findById(id).orElse(null)
     }
 
     @PostMapping
